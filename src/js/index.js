c3add9341fc13a025746263338180ea1355d353e
uiuxkimabe <uiuxkimabe@gmail.com>
2024-07-29 07:41:56 +0700

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+// countdown
+
+const days = document.querySelector('.days')
+const hour = document.querySelector('.hour')
+const minute = document.querySelector('.minute')
+const second = document.querySelector('.second')
+const timeBlock = document.querySelectorAll('.time')
+const video = document.querySelector('#video')
+
+
+// Selection tahun
+const currentYear = new Date().getFullYear();
+
+// Waktu Ulang Tahun
+const birthdayTime = new Date(`August 13 ${currentYear} 00:00:00`)
+
+
+// Update Countdown Time
+
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = birthdayTime - currentTime
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24)
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24
+    const m = Math.floor(diff / 1000 / 60) % 60
+    const s = Math.floor(diff / 1000) % 60
+
+    // console.info(d,h,m,s)
+
+    days.innerHTML = d < 1 ? 0 : d
+    hour.innerHTML = h < 1 ? 0 : h
+    minute.innerHTML = m < 1 ? 0 : m
+    second.innerHTML = s < 1 ? 0 : s
+
+    if (m == 0 && s <= 10) {
+        second.style.fontSize = '10rem'
+        timeBlock[0].style.display = 'none'
+        timeBlock[1].style.display = 'none'
+        timeBlock[2].style.display = 'none'
+    } else if (m <= 0 && s <= 1) {
+        video.classList.add('show')
+        // console.info(birthdayTime)
+    }
+
+}
+
+setInterval(updateCountdown, 1000)
