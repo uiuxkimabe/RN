575644d755f817f0ab07e698663085ad8ac592f9
uiuxkimabe <uiuxkimabe@gmail.com>
2024-08-09 08:14:54 +0700

    moment slide

--- src/js/index.js
+++ src/js/index.js
@@ -19,11 +19,11 @@ const nextPage = document.querySelector('.nextPage')
 // Function typing
 function typing() {
     let typed = new Typed('#element', {
-        strings: ['<i>Suatu hari, tepatnya pada 13 Agustus</i>','lahirlah seorang wanita cantik di sebuah daerah di Bogor','Panggilannya <strong>Rani</strong>','Suatu hari, Rani diundang ke kahyangan','Katanya dia di panggil oleh raja Awan.','Sampai disana Rani ditanya tentang hari paling indah','"Hari apa yang paling indah Rani ? Kalo kamu bisa jawab kamu akan mendapat hadiah"','Hadiah apa wahai raja ?, Tanya Rani','Dia lah obat kebahagian','Di mana ada hadiah semacam itu ?, tanya Rani pada raja awan','Tentu ada, dia obat yang akan membuat mu kuat, sehat dan penuh semangat','Dimana saya bisa dapat itu raja ?, sahut Rani','Ada di dalam keranjang !','Ketika Rani membuka dia menemukan puyer bintang 7 di dalam keranjang, lalu tersenyum dan bertanya pada raja, Mengapa obat semacam ini jadi obat bahagia ?','Karena obat ini bisa membuatmu bingung, tersenyum, dan menjadikan hari ini menjadi hari paling indah, hari saat kamu tambah usia dengan hati bahagia.','Selamat tambah usia Raniah Nurfadilah','It\'s mean a lot, the world feel better, ketika kamu bahagia di hari lahirmu.','Wish you all the best... 🙏🙏🙏'],
+        strings: ['<i>Suatu hari, tepatnya pada 13 Agustus</i>','<i>lahirlah seorang wanita cantik di sebuah daerah di Bogor</i>','<i>Panggilannya <strong>Rani</strong><i/>','<i>Suatu hari, Rani diundang ke kahyangan</i>','<i>Katanya dia di panggil oleh raja Awan.</i>','<i>Sampai disana Rani ditanya tentang hari paling indah<i/>','<i>"Hari apa yang paling indah Rani ? Kalo kamu bisa jawab kamu akan mendapat hadiah"</i>','<i>Hadiah apa wahai raja ?, Tanya Rani<i/>','<i>Dia lah obat kebahagian</i>','<i>Di mana ada hadiah semacam itu ?, tanya Rani pada raja awan</i>','<i>Tentu ada, dia obat yang akan membuat mu kuat, sehat dan penuh semangat<i/>','<i>Dimana saya bisa dapat itu raja ?, sahut Rani</i>','<i>Ada di dalam keranjang !</i>','<i>Ketika Rani membuka, dia menemukan puyer bintang 7</i>', '<i>lalu bertanya pada raja, Mengapa puyer bintang 7 ?</i>','<i>Karena obat ini bisa membuatmu bingung, tersenyum, dan menjadikan hari ini menjadi hari paling indah, hari saat Rani tambah usia dengan hati bahagia.</i>','<i>Selamat tambah usia Raniah Nurfadilah...</i>','<i>It\'s mean a lot, the world feel better, ketika kamu bahagia di hari lahirmu.</i>','<i>Sehat selalu, sukses dan panjang umur</i>','<i>Wish you all the best... 🙏🙏🙏</i>'],
         typeSpeed: 30,
         fadeOut: true,
         loop: true,
-        backDelay: 1500,
+        backDelay: 1800,
         showCursor: false
     });
     return typed
